701d87ce8716f78052bef78726e7e66b5537dd42
Leegunjoo <rjswn1595@gmail.com>
2020-11-04 14:40:58 +0900

    함수

---
+++ js/ex7.js
@@ -0,0 +1,21 @@
+//let accessAllowed = (age > 18) ? true : false;
+
+function showMessage() {
+    alert( '안녕하세요!' );
+  }
+  function name(parameters){
+      //함수 본문
+  }
+
+  function addToNumbers(a, b){
+      return a + b;
+  }
+  let sayHi = function(){
+      alert("Hello");
+  }
+
+  function sayHi() {
+    alert( "Hello" );
+  }
+  
+  alert( sayHi ); // 함수 코드가 보임
